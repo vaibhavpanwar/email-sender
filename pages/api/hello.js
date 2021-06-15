2ec6459ccbbbad5236f96fce40376fd750ca43bd
vaibhav <vaibhavpanwar1402@gmail.com>
2021-06-15 17:49:18 +0530

    initial commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,33 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const sgMail = require("@sendgrid/mail");
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+  const body = req.body;
+  const customMessage = `
+  Name: ${body.name}\r\n
+  Email: ${body.email}\r\n
+  Message: ${body.message}
+`;
+
+  sgMail.setApiKey(process.env.API_KEY);
+  const msg = {
+    to: "vaibhavpanwar1402@gmail.com", // Change to your recipient
+    from: "vaibhav@grivety.tech", // Change to your verified sender
+    subject: "Test Subject",
+    text: customMessage,
+    html: customMessage.replace(/\r\n/g, "<br>"),
+  };
+
+  try {
+    sgMail
+      .send(msg)
+      .then(() => {
+        res.status(200).send("ok");
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500);
+      });
+  } catch (error) {
+    res.status(500);
+  }
+};
